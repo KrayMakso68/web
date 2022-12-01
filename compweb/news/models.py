fb922fcdf03fed553729c7f2ee8ac34e566214eb
KrayMakso68 <makson6803@gmail.com>
2022-12-01 20:44:04 +0300

    add News in site

--- compweb/news/models.py
+++ compweb/news/models.py
@@ -7,3 +7,6 @@ class Articles(models.Model):
     data = models.DateTimeField('Дата публикации')
     def __str__(self):
         return self.title
+    class Meta:
+        verbose_name = 'Новость'
+        verbose_name_plural = 'Новости'
--- compweb/news/views.py
+++ compweb/news/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from .models import Articles
 
 # Create your views here.
 def news_index(request):
+    news = Articles.objects.order_by('-data')
     data = {
         'title': 'Новости',
+        'news' : news,
     }
     return render(request, 'news/news_home.html', data)
